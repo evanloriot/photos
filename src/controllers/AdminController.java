6057e3390ca8544fb337bda5d5ae41bcdf840a42
Evan J. Loriot <ejl132@rutgers.edu>
2017-11-16 21:41:57 -0500

    Made a user model. Added some functionality to AdminController.

--- src/controllers/AdminController.java
+++ src/controllers/AdminController.java
@@ -1,10 +1,13 @@
 package controllers;
 
+import java.util.ArrayList;
+
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.stage.Stage;
+import models.User;
 
 public class AdminController {
 	@FXML
@@ -14,11 +17,32 @@ public class AdminController {
 	@FXML
 	Button delete;
 	@FXML
-	ListView<String> users;
+	ListView<User> users;
 	
-	ObservableList<String> obsList;
+	ObservableList<User> obsList;
 	
 	public void start(Stage mainStage) {
 		
+	}
+	
+	public ArrayList<User> getUsers(){
+		//implement get users from db
+		
+		//temp
+		ArrayList<User> users = new ArrayList<User>();
+		users.add(new User("evan"));
+		users.add(new User("temp1"));
+		users.add(new User("temp2"));
+		users.add(new User("temp3"));
+		users.add(new User("temp4"));
+		return users;
+	}
+	
+	public void deleteUser(User user) {
+		//implement delete user from db
+	}
+	
+	public void addUser(String username) {
+		//implement add user to db
 	}
 }
